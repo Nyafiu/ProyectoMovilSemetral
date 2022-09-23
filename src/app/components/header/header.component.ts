2de64819848a9e9e40f03496521b470364edc9c7
majobc02 <majobc02@gmail.com>
2022-09-23 11:39:58 -0300

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input() pageTitle :string;
+  @Input() isNotHome :boolean;
 
+  constructor(private router:Router) { }
+
+  goAbout(){
+    this.router.navigate(['/about']);
+  }
+  
   ngOnInit() {}
 
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  pageTitle = 'Home';
+  isNotHome = false;
+
   constructor() {}
 
 }
